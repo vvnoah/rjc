00d8aa9072acd68b38d390f12a3228bfcae0b81e
vvnoah <vvnoah@hotmail.com>
2023-11-17 10:24:01 +0100

    Testing media control over mqtt

--- ESP32/src/main.cpp
+++ ESP32/src/main.cpp
@@ -1,18 +1,47 @@
 #include <Arduino.h>
 #include <BleMouse.h>
+#include <WiFi.h>
+#include <PubSubClient.h>
 
 BleMouse mouse("Joystick Controller", "UCLL");
+WiFiClient wifi_client;
+PubSubClient mqtt_client(wifi_client);
+
+const *char SSID = "";
+const *char PASSWORD = "";
+const char* MQTT_SERVER = ""; // IP
+const int MQTT_PORT = 3000;
+const char* MQTT_USER = "ucll";
+const string MQTT_CLIENTID = "ESP32-" + String(random(0xffff), HEX);
 
 void move_mouse_with_joystick();
+void connect_mqtt();
 
 void setup() {
   Serial.begin(9600);
   mouse.begin();
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(SSID, PASSWORD);
+  while (WiFi.status( != WL_CONNECTED)) {
+    Serial.print(".");
+    delay(500);
+  }
+  Serial.println(WiFi.localIP());
 }
 
 void loop() {
   if(mouse.isConnected()) {
     move_mouse_with_joystick();
+  }
+
+  if(Wifi.status() == WL_CONNECTED) {
+    if(!mqtt_client.connected()) {
+      connect_mqtt();
+    }
+    mqtt_client.loop();
+
+    // IF MODE MEDIA, GET BUTTON INPUT AND PUBLISH MEDIA COMMANDS.
+    mqtt_client.publish("/media/audio_vol_up");
   }
 
   delay(35);
@@ -44,4 +73,8 @@ void move_mouse_with_joystick() {
   Serial.println(mapped_joystick_y);
 
   mouse.move(mapped_joystick_x, mapped_joystick_y);
+} 
+
+void connect_mqtt() {
+  mqtt_client.setServer()
 }
