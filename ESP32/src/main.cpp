92ec433036e3480440b0deb2de2acf07e8cfcb89
vvnoah <vvnoah@hotmail.com>
2023-11-20 22:43:22 +0100

    transmitting joystick position data using websocket

--- ESP32/src/main.cpp
+++ ESP32/src/main.cpp
@@ -1,71 +1,48 @@
 #include <Arduino.h>
 #include <WiFi.h>
-#include <PicoMQTT.h>
+#include <WebSocketsServer.h>
 
 #include "modules/joystick/rjc_joystick.h"
 
 WiFiClient wifi_client;
-PicoMQTT::Server mqtt;
+WebSocketsServer websocket = WebSocketsServer(81);
 
 static const char*  SSID           = "KotAzurPloeg+2";
 static const char*  PASSWORD       = "";
-static const int    MQTT_PORT      = 1883;
-static const char*  MQTT_USER      = "UCLL";
-static const String mqtt_ID = "ESP32-" + String(random(0xffff), HEX);
 
-void mqtt_callback(char* topic, byte* payload, unsigned int length);
+void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t length) {
+    // Handle events here
+}
 
-
-void setup()
-{
+void setup() {
   Serial.begin(9600);
   WiFi.mode(WIFI_STA);
   WiFi.begin(SSID, PASSWORD);
-  Serial.println ("Connecting to wifi ");
-  while (WiFi.status() != WL_CONNECTED) 
-  {
-      delay(500);
-      Serial.print(".");
+  Serial.println("Connecting to WiFi ");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
   }
   Serial.printf("WiFi connected, IP: %s\n", WiFi.localIP().toString().c_str());
-  mqtt.begin();
+  websocket.begin();
 }
 
-void loop()
-{
+void loop() {
   JoystickPosition joystick_position = RJC_JOYSTICK::get_mapped_position(-10, 10);
-  if (joystick_position.y > 0) 
-  {
-    if(mqtt.publish("volume/up", "1"))
-    {
-      Serial.println("[mqtt]: volume up");
-    }
-    else
-    {
-      Serial.println("[mqtt_error]: failed to publish volume up");
-    }
-  }
-  else if (joystick_position.y < 0)
-  {
-    if(mqtt.publish("volume/down", "1"))
-    {
-      Serial.println("[mqtt]: volume down");
-    }
-    else
-    {
-      Serial.println("[mqtt_error]: failed to publish volume down");
-    }
+
+  // Example: Send joystick position over WebSocket when the joystick is moved
+  if (joystick_position.y != 0 || joystick_position.y != 0) {
+    // Create a JSON message using printf
+    char messageBuffer[255];
+    snprintf(messageBuffer, sizeof(messageBuffer), R"({"type":"joystick","data":{"x":%d},{"y":%d}})", joystick_position.x, joystick_position.y);
+
+
+    // Send the message to all connected clients
+    websocket.broadcastTXT(messageBuffer);
+
+    Serial.println("Sent joystick position over WebSocket: " + (char)*messageBuffer);
   }
 
-  mqtt.loop();
+  websocket.loop();
   delay(35);
-}
-
-void mqtt_callback(char* topic, byte* payload, unsigned int length) {
-    // Handle received messages
-    Serial.print("Received Message - Topic: ");
-    Serial.println(topic);
-    Serial.print("Payload: ");
-    Serial.write(payload, length);
-    Serial.println();
 }
--- back-end/server.js
+++ back-end/server.js
@@ -1,54 +1,20 @@
-// PC Code
-const mqtt = require('mqtt');
-const robot = require('robotjs');
+let WebSocketClient = require('websocket').client;
 
-const protocol = 'mqtt';
-const host = '192.168.0.218';
-const port = '1883';
-const clientId = `mqtt_${Math.random().toString(16).slice(3)}`;
-const connectUrl = `${protocol}://${host}:${port}`;
-const username = 'ucll';
-const password = '';
-const topicsToSubscribe = ['volume/up', 'volume/down', 'other/topic'];
+let client = new WebSocketClient();
 
-const client = mqtt.connect(connectUrl, {
-  clientId,
-  clean: true,
-  connectTimeout: 4000,
-  username: username,
-  password: password,
-  reconnectPeriod: 1000,
-});
-
-client.on('connect', () => {
-  console.log('Connected to ESP32 MQTT Broker');
-  
-  // Subscribe to multiple topics
-  topicsToSubscribe.forEach((topic) => {
-      client.subscribe(topic, () => {
-          console.log(`Subscribed to topic '${topic}'`);
-      });
+client.on('connect', function(connection) {
+  console.log('WebSocket Client Connected');
+  connection.on('error', function(error) {
+      console.log("Connection Error: " + error.toString());
+  });
+  connection.on('close', function() {
+      console.log('echo-protocol Connection Closed');
+  });
+  connection.on('message', function(message) {
+      if (message.type === 'utf8') {
+          console.log("Received: '" + message.utf8Data + "'");
+      }
   });
 });
 
-client.on('message', (topic, payload) => {
-    console.log('Received Message:');
-    console.log('- Topic:', topic);
-    console.log('- Message:', payload.toString());
-
-    if(topic === "volume/up") {
-      volume_up();
-    }
-
-    if(topic === "volume/down") {
-      volume_down();
-    }
-});
-
-function volume_up() {
-  robot.keyTap("audio_vol_up");
-}
-
-function volume_down() {
-  robot.keyTap("audio_vol_down");
-}
+client.connect('ws://192.168.0.218:81/');
