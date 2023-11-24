dec7c66ec98b21b6a42d0783c646a7b58e28e24a
vvnoah <vvnoah@hotmail.com>
2023-11-24 09:19:28 +0100

    Control mouse with joystick over WebSockets

--- ESP32/src/main.cpp
+++ ESP32/src/main.cpp
@@ -1,14 +1,15 @@
 #include <Arduino.h>
 #include <WiFi.h>
 #include <WebSocketsServer.h>
+#include <ArduinoJson.h>
 
 #include "modules/joystick/rjc_joystick.h"
 
 WiFiClient wifi_client;
 WebSocketsServer websocket = WebSocketsServer(81);
 
-static const char*  SSID           = "KotAzurPloeg+2";
-static const char*  PASSWORD       = "";
+static const char*  SSID           = "BillyTheRobot";
+static const char*  PASSWORD       = "eloict1234";
 
 void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t length) {
     // Handle events here
@@ -31,16 +32,23 @@ void loop() {
   JoystickPosition joystick_position = RJC_JOYSTICK::get_mapped_position(-10, 10);
 
   // Example: Send joystick position over WebSocket when the joystick is moved
-  if (joystick_position.y != 0 || joystick_position.y != 0) {
-    // Create a JSON message using printf
-    char messageBuffer[255];
-    snprintf(messageBuffer, sizeof(messageBuffer), R"({"type":"joystick","data":{"x":%d},{"y":%d}})", joystick_position.x, joystick_position.y);
+  if (joystick_position.x != 0 || joystick_position.y != 0) {
+    // Create a JSON document
+    StaticJsonDocument<100> jsonDocument;
 
+    // Populate the JSON document
+    jsonDocument["type"] = "cursor";
+    jsonDocument["data"]["x"] = joystick_position.x;
+    jsonDocument["data"]["y"] = joystick_position.y;
+
+    // Serialize the JSON document to a String
+    String message;
+    serializeJson(jsonDocument, message);
 
     // Send the message to all connected clients
-    websocket.broadcastTXT(messageBuffer);
+    websocket.broadcastTXT(message);
 
-    Serial.println("Sent joystick position over WebSocket: " + (char)*messageBuffer);
+    Serial.println("Sent joystick position over WebSocket: " + message);
   }
 
   websocket.loop();
--- back-end/server.js
+++ back-end/server.js
@@ -1,20 +1,46 @@
 let WebSocketClient = require('websocket').client;
+const robot = require('robotjs');
 
 let client = new WebSocketClient();
 
 client.on('connect', function(connection) {
-  console.log('WebSocket Client Connected');
+  console.log('WebSocket Connected');
   connection.on('error', function(error) {
       console.log("Connection Error: " + error.toString());
   });
   connection.on('close', function() {
-      console.log('echo-protocol Connection Closed');
+      console.log('Connection Closed.');
   });
   connection.on('message', function(message) {
-      if (message.type === 'utf8') {
-          console.log("Received: '" + message.utf8Data + "'");
+    if (typeof message.utf8Data === 'string') {
+        try {
+          const data = JSON.parse(message.utf8Data);
+
+          const type = data.type;
+          const x = data.data.x;
+          const y = data.data.y;
+
+          if(type == "cursor") {
+            moveMouseWithJoystick(x, y);
+          } else if (type == "scroll") {
+            robot.scrollMouse(x, y);
+          }
+
+          console.log(`[ws]: ${type}: ${x}, ${y}`);
+        } catch (error) {
+          console.error('Error parsing JSON:', error);
+        }
+      } else {
+        console.error('Received non-string data:', message.utf8Data);
       }
   });
 });
 
-client.connect('ws://192.168.0.218:81/');
+client.connect('ws://192.168.1.104:81/');
+
+function moveMouseWithJoystick(x, y) {
+  let pos = robot.getMousePos();
+  pos.x += x + 5;
+  pos.y += y + 5;
+  robot.moveMouse(pos.x, pos.y);
+}
